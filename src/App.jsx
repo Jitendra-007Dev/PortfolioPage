6046d9af64d35f43a34ddfb35d5bd7c15d7f2e4b
JACK99-JACK99 <jitenmaurya0@gmail.com>
2023-04-27 15:28:41 +0530

    refactoring all websites

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './Components/Header';
-import Sidebar from './Components/Sidebar';
-import routes from './DataLibrary/PageData'
+import { Header } from './components/Global/Header';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 
@@ -13,33 +11,14 @@ function App() {
 		<>
 			<Router>
 				<Header />
-				{/* <Sidebar /> */}
 				<Routes>
-					{routes.map((route, index) => (
-						<Route
-							key={index}
-							path={route.path}
-							exact={route.exact}
-							element={route.sidebar}
-						/>
-					))}
+					{/* <Route
+						key={index}
+						path={route.path}
+						exact={route.exact}
+						element={route.sidebar}
+					/> */}
 				</Routes>
-
-				{/* <div style={{ flex: 1, padding: "10px" }}>
-						<Routes>
-							{routes.map((route, index) => (
-								// Render more <Route>s with the same paths as
-								// above, but different components this time.
-								<Route
-									key={index}
-									path={route.path}
-									exact={route.exact}
-									children={<route.main />}
-								/>
-							))}
-						</Routes>
-					</div> */}
-				{/* </div> */}
 			</Router>
 		</>
 	)
