270d5918c4ef84fe0aa13d5610374443d8d9b201
JACK99-JACK99 <jitenmaurya0@gmail.com>
2023-04-27 21:36:16 +0530

    added fonts and new packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "./styles/css/bootstrap.css"
+import "./styles/vendors/linericon/style.css"
+import "./styles/css/themify-icons.css"
+import "./styles/vendors/owl-carousel/owl.theme.default.min.css"
+import "./styles/vendors/fontawesome/css/all.min.css"
+import "./styles/vendors/owl-carousel/owl.carousel.min.css"
+import "./styles/vendors/nice-select/css/nice-select.css"
+import "./styles/css/style.css"
 import { Header } from './components/Global/Header';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
